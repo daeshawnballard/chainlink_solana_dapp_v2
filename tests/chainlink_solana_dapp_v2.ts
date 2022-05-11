b5c582427abdca342294b1f7a0ca2d4216382e26
imdaeshawn <imdaeshawn@gmail.com>
2022-05-11 18:38:56 -0400

    deleted 12 and 13

--- tests/chainlink_solana_dapp_v2.ts
+++ tests/chainlink_solana_dapp_v2.ts
@@ -9,24 +9,21 @@ const CHAINLINK_FEED = "HgTtcbcmp5BeThax5AU8vg4VwK79qAvAKKFMs8txMLW6";
 const CHAINLINK_PROGRAM_ID = "31xNV613uNqutXFi35nCCbGuDUbr7p26SMJZoJFLKrAk";
 
  describe("chainlink_solana_dapp_v2", () => {
-  const provider = anchor.AnchorProvider.env();
-  anchor.setProvider(provider);
   const program = anchor.workspace.ChainlinkSolanaDappV2 as Program<ChainlinkSolanaDappV2>;
    it('Queries SOL/USD Price Feed', async() => { 
     console.log(program)
     const resultAccount = anchor.web3.Keypair.generate();
-   
-     const tx = await program.methods.execute()
+      const tx = await program.methods.execute()
         .accounts({
-          resultAccount: resultAccount. publicKey,
-          user: provider.wallet.publicKey, 
-          systemProgram: anchor.web3.SystemProgram.programId, 
-          chainlinkFeed: CHAINLINK_FEED, 
+          resultAccount: resultAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          chainlinkFeed: CHAINLINK_FEED,
           chainlinkProgram: CHAINLINK_PROGRAM_ID
-
-      })
+        })
         .signers([resultAccount])
         .rpc()
+
       console.log(tx)
     const latestPrice = await program.account.resultAccount.fetch(resultAccount.publicKey)
     //dividing into the units that will give us usd
